1df749b0dcfda4b7ee8b0a33952ac9186541e4d1
tippi fifestarr <tippi@boober.org>
2020-08-31 21:33:06 -0500

    Version 1.2
    Added dadeuce player, logo, real email, 3rd row and training booklet seed image.

--- cardNames.js
+++ cardNames.js
@@ -32,6 +32,22 @@ export default
     {
       "name" : "Alex Kerr",
       "picture" : "https://media-exp1.licdn.com/dms/image/C5603AQFCywtmxsfJYg/profile-displayphoto-shrink_400_400/0?e=1601510400&v=beta&t=pXZd-Ru4Tpu8sFL2jPuYN1MFWt_wf9SB1KcatsZGz_A"
+    },
+    {
+      "name" : " Dexter the Golden",
+      "picture" : "https://happyvalentinesday2016i.com/sites/default/files/styles/puppy_full/public/puppy-photo/puppies_for_sale_in_pa_OHPH3-15328_2-101890745.jpg?itok=taTzh4fn"
+    },
+    {
+      "name" : "Seoul Music Week",
+      "picture" : "./images/SMWeek.jpg"
+    },
+    {
+      "name" : "Stan 2019/05/17",
+      "picture" : "./images/stan20190517.jpg"
+    },
+    {
+      "name" : "Zing 2019/05/15",
+      "picture" : "./images/zing20190515.jpg"
     }
   ]
 }
