a1d4450f3c2d319b9d580c194ae2fb8a4a2ccc0c
tippi-fifestarr <62179036+tippi-fifestarr@users.noreply.github.com>
2020-09-23 20:34:46 -0500

    1.6 failblog
    the failed version with tones of txt and now dan's deleting everything

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 import cardsArr from "./cardNames.js"
 
+let cardsArrTwin = [cardsArr];
+console.log (cardsArrTwin)
+console.log(cardsArr)
 const cardContainer = document.querySelector('#cards-container');
 const cardChoiceContainer = document.querySelector('#card-choice-container')
+
+const colorsArr = ['red', 'blue', 'green', 'orange', 'purple', 'black','white']
 let isChosen = false;
+let twinmode = false;
 let chosenCard;
 
+// function
+
 // var item = items[Math.floor(Math.random() * items.length)]
+
+window.onclick = myFunction;
+// https://www.w3schools.com/jsref/event_onclick.asp
+// If the user clicks in the window, set the background color of <body> to yellow
+function myFunction() {
+  const randomNum = Math.floor(Math.random() * Math.floor(colorsArr.length))
+  document.getElementsByTagName("BODY")[0].style.background = colorsArr[randomNum]
+  // document.getElementsByTagName("BODY")[0].style.backgroundColor = "yellow";
+}
 
 document.getElementById("Button").onclick = function() {
   // check for an empty array : 
@@ -16,6 +33,7 @@ document.getElementById("Button").onclick = function() {
       const charCard = document.createElement("div");
       charCard.classList.add("card")
       charCard.id = `${removedE[0].name}`
+      // make the card flippable
       charCard.onclick = function(e){
         charCard.classList.toggle("hidden");
         isChosen ? null : isChosenFunc(e)
@@ -60,9 +78,66 @@ document.getElementById("Button").onclick = function() {
   } else {
     alert("the array is now empty");
     document.querySelector('#Button').remove()
-
   }
-}
+      // if(twinmode==true){
+      //   console.log("twinmode")
+      // // }
+    document.getElementById("twin").onclick = function(){
+        let twinmode=true
+        console.log("i let it be true")
+        // select the play area (including images and selected character) and = copythat
+        let copythat = document.querySelector('#play-table');
+        // create thatcopy which is a .cloneNode(deep = troo)
+        let thatcopy = copythat.cloneNode(true);
+        // for loop through each child element, add the same onclick as up there.
+        // document.getElementById("thatcopy") = function()??
+        console.log(thatcopy.children)
+        console.log(thatcopy.childNodes[3].childNodes)
+        // console.log(thatcopy.childNodes.values)
+        // var kids = thatcopy.children;
+        // this selects all the .card classes in the and makes it kids
+        // const kids = thatcopy.childNodes[3].querySelectorAll(".card")
+        const kids = thatcopy.childNodes[3].childNodes
+        
+        console.log(kids)
+        // var kids1 = kids.childNode;
+        // console.log("kidcheck")
+        // console.log(kids1)
+        // find the array of objs, queryselector.all ? has a certain class "car"
+        // document.querySelectorAll(".card")
+        // console.log()
+        // closest to find the parent
+        // .map .filter .reduce are essential
+        // for (var i = 0; i < kids.length; i++) {
+          kids.map(kid =>{
+            kid.onclick = function(e){
+              // console.log("the child works")
+              kid.classList.toggle("hidden");
+              console.log(tableChild)
+            }
+          }) 
+        // }
+          //     var tableChild = kids[i];
+          //     console.log("the child works")
+                
+          //     tableChild.onclick = function(e){
+          //       // console.log("the child works")
+                
+          //       tableChild.classList.toggle("hidden");
+          //       console.log(tableChild)
+          //     }
+          // }
+            // Do stuff
+          
+        // fancy dancy option: modulate the function so you can use it in multiple places ie. handleClick()
+        thatcopy.id = 'twin-copy';
+        document.body.appendChild(thatcopy);
+      }
+  }
+
+// }
+//   }
+// }
 // cardsArr.cards.forEach(card => {
 //   const charCard = document.createElement("div");
 //   charCard.classList.add("card")
@@ -106,3 +181,4 @@ const isChosenFunc = (e) => {
   cardChoiceContainer.appendChild(chosenCharCard)
   isChosen = true;
 }
+
